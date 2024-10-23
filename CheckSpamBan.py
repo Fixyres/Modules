87615b10c0ffb5c0dadfec38cdd99d329959986b
Артем <158840682+Fixyres@users.noreply.github.com>
2024-10-23 20:16:51 +0300

    Add CheckSpamBan.py

---
+++ CheckSpamBan.py
@@ -0,0 +1,70 @@
+# Proprietary License Agreement
+
+# Copyright (c) 2024-29 CodWiz
+
+# Permission is hereby granted to any person obtaining a copy of this software and associated documentation files (the "Software"), to use the Software for personal and non-commercial purposes, subject to the following conditions:
+
+# 1. The Software may not be modified, altered, or otherwise changed in any way without the explicit written permission of the author.
+
+# 2. Redistribution of the Software, in original or modified form, is strictly prohibited without the explicit written permission of the author.
+
+# 3. The Software is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement. In no event shall the author or copyright holder be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the Software or the use or other dealings in the Software.
+
+# 4. Any use of the Software must include the above copyright notice and this permission notice in all copies or substantial portions of the Software.
+
+# 5. By using the Software, you agree to be bound by the terms and conditions of this license.
+
+# For any inquiries or requests for permissions, please contact codwiz@yandex.ru.
+
+# ---------------------------------------------------------------------------------
+# Name: CheckSpamBan
+# Description: Check spam ban for your account.
+# Author: @hikka_mods
+# ---------------------------------------------------------------------------------
+# meta developer: @hikka_mods
+# scope: CheckSpamBan
+# scope: CheckSpamBan 0.0.1
+# ---------------------------------------------------------------------------------
+
+from .. import loader, utils
+from ..utils import answer
+from telethon.tl.types import Message
+
+__version__ = (1, 0, 0)
+
+
+@loader.tds
+class SpamBanCheckMod(loader.Module):
+    """Check spam ban for your account."""
+
+    strings = {
+        "name": "CheckSpamBan",
+        "svo": "Your account is free from any restrictions.",
+        "good": "<b>Everything is fine!You don't have a spam ban.</b>",
+        "spamban": "<b>Unfortunately, your account has received a spam ban...\n\n{kk}\n\n{ll}</b>",
+    }
+
+    strings_ru = {
+        "svo": "Ваш аккаунт свободен от каких-либо ограничений.",
+        "good": "<b>Все прекрасно!\nУ вас нет спам бана.</b>",
+        "spamban": "<b>К сожалению ваш аккаунт получил спам-бан...\n\n{kk}\n\n{ll}</b>",
+    }
+
+    @loader.command(
+        ru_doc="Проверяет вашу учетную запись на спам-бан с помощью бота @SpamBot",
+        en_doc="Checks your account for spam ban via @SpamBot bot",
+    )
+    async def spambancmd(self, message: Message):
+        async with self._client.conversation("@SpamBot") as conv:
+            msg = await conv.send_message("/start")
+            r = await conv.get_response()
+            if r.text == self.strings("svo"):
+                text = self.strings("good")
+            else:
+                response_lines = r.text.split("\n")
+                kk = response_lines[2]
+                ll = response_lines[4]
+                text = self.strings("spamban").format(kk=kk, ll=ll)
+            await msg.delete()
+            await r.delete()
+            await answer(message, text)
