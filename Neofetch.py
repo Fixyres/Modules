cd00135bd425f51951ebf59c338fab27d83b78ea
Артем <158840682+Fixyres@users.noreply.github.com>
2024-10-23 11:39:17 +0300

    Add Neofetch.py

---
+++ Neofetch.py
@@ -0,0 +1,23 @@
+# meta developer: @htmIpage
+
+import subprocess
+import traceback
+from .. import loader, utils
+
+@loader.tds
+class NeofetchMod(loader.Module):
+    strings = {"name": "Neofetch"}
+
+    async def neofetchcmd(self, message):
+        """Запустить команду neofetch"""
+        try:
+            result = subprocess.run(["neofetch", "--stdout"], capture_output=True, text=True)
+            output = result.stdout
+
+            if result.returncode != 0:
+                await utils.answer(message, f"<b>Ошибка выполнения команды neofetch:</b>\n{result.stderr}")
+                return
+
+            await utils.answer(message, f"<pre>{utils.escape_html(output)}</pre>")
+        except Exception:
+            await utils.answer(message, f"<b>Ошибка:</b>\n<pre>{traceback.format_exc()}</pre>")
