a8f0df3c8bdce7e7ae7011cbe51398c431564e4f
Артем <158840682+Fixyres@users.noreply.github.com>
2024-11-08 20:16:34 +0200

    Add BOLD.py

---
+++ BOLD.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# meta developer: @Foxy437
+# Попросил сделать: @qequqo
+
+from .. import loader, utils
+
+@loader.tds
+class BoldMod(loader.Module):
+    """Модуль для авто замены шрифта на жирный."""
+    strings = {"name": "Bold"}
+    
+    async def client_ready(self, client, db):
+        self.client = client
+        self.db = db
+        self.db.set(self.strings["name"], "bold_enabled", False)
+
+    async def on_boldcmd(self, message):
+        """Включить авто замену шрифта на жирный."""
+        self.db.set(self.strings["name"], "bold_enabled", True)
+        await utils.answer(message, "СЭР ДА СЭР!")
+
+    async def off_boldcmd(self, message):
+        """Выключить авто замену шрифта на жирный."""
+        self.db.set(self.strings["name"], "bold_enabled", False)
+        await utils.answer(message, "СЭР ДА СЭР!")
+
+    async def watcher(self, message):
+        if self.db.get(self.strings["name"], "bold_enabled"):
+            if message.out:
+                bold_text = f"<b>{message.text}</b>"
+                await message.edit(bold_text)
