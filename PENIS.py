153135105e3f5dea8f1d62b6f73efb204192f80a
Artem <foxyy437@gmail.com>
2025-01-20 15:01:35 +0200

    Upload PENIS.py

---
+++ PENIS.py
@@ -0,0 +1,12 @@
+
+# meta developer: @xensidemod
+# requires: penis
+
+from .. import loader, utils
+import penis
+
+@loader.tds
+class PenisMod(loader.Module):
+    """Just Penis"""
+
+    strings = {"name": "Penis"}
