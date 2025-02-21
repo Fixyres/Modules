9f2e46b62388325b1decb5e9f635b01efb057332
Artem <foxyy437@gmail.com>
2025-02-21 18:35:54 +0200

    AWWW

---
+++ 195d8ae17b77b1e3d98c21732e0227c5dadfa098ce59e5d28841052edab01e1f.py
@@ -0,0 +1,32 @@
+#     ___    __ __ ____
+#    /   |  / //_//  _/
+#   / /| | / ,<   / /  
+#  / ___ |/ /| |_/ /   
+# /_/  |_/_/ |_/___/   
+#                      
+#     __  _______  ____  _____
+#    /  |/  / __ \/ __ \/ ___/
+#   / /|_/ / / / / / / /\__ \ 
+#  / /  / / /_/ / /_/ /___/ / 
+# /_/  /_/\____/_____//____/
+#
+# meta developer: @aki_modules
+
+from .. import loader, utils
+import telethon
+
+class WalletCheckMod(loader.Module):
+    strings = {'name': 'WalletCheck'}
+
+    async def walletcmd(self, message: telethon.tl.types.Message):
+        """Отправляет ваш баланс в @send"""
+        bot_username = "@send"
+
+        async with message.client.conversation(bot_username) as conv:
+            sent_message = await conv.send_message("/wallet")
+            response = await conv.get_response()
+
+            await sent_message.delete()
+            await response.delete()
+
+        await utils.answer(message, response.text)
