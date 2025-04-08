7c329c0b91b5aaac8a63a1935183981d269e7b5b
Artem <foxyy437@gmail.com>
2025-04-08 21:53:47 +0300

    Update rnd.py

--- rnd.py
+++ rnd.py
@@ -1,11 +1,7 @@
-from .. import loader
-import random, string
-def r(l=8): return ''.join(random.choices(string.ascii_lowercase, k=l))
-n = r()
-@loader.tds
-class M(loader.Module):
-    strings = {"name": n}
-    async def qjdbmfuzcmd(self, message): 
-        pass
-M.__name__ = n
-globals()[n] = M
+from .. import loader as l, utils as u
+import random as r, string as s
+g,n,d=r.randint,lambda:''.join(r.choices(s.ascii_letters,k=g(10,18))),lambda:' '.join([n()for _ in range(g(2,5))])
+class C(l.Module):
+    __doc__=d();strings={"name":n()}
+    for _ in range(g(1,5)):exec(f"async def {n().lower()}cmd(s,m):\n    \"\"\"{d()}\"\"\"\n    await u.answer(m,'{d()}')")
+C.__name__=n();globals()[C.__name__]=C
